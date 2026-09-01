13ee3d0d5e11c2145eaf195e68549f7a435458b9
Kaeru <thefrogkaeru@gmail.com>
2026-09-01 12:36:19 +0200

    init

---
+++ resources/aspect.gd
@@ -0,0 +1,7 @@
+class_name Aspect
+extends Resource
+
+@export var id: String
+@export var display_name: String
+@export var primary_color : Color = Color.WHITE
+@export var secondary_color : Color = Color.WHITE
---
+++ resources/class.gd
@@ -0,0 +1,6 @@
+class_name SBURBClass
+extends Resource
+
+@export var id: String
+@export var display_name: String
+@export_enum("Active", "Passive") var mode: String
